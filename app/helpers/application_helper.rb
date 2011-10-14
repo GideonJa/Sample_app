e17ce2ffc3f14ff4753a09f30d181f4ef85bc2bc
Gideon jadlovker <gideonj1@gmail.com>
2011-10-14 10:03:27 -0400

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     else "Ruby on Rails Tutorial Sample App | #{@title}"
   end
 end
+
+def logo
+  image_tag "logo.png", :alt =>  "Tutorial Logo", :class => "round"
+end
 end
