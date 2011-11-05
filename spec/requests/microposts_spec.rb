2cca3ec07dc92dac88abe33eadf87ceb52ce9a68
Gideon jadlovker <gideonj1@gmail.com>
2011-11-05 19:55:05 -0400

    finished MP and int test

---
+++ spec/requests/microposts_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Microposts" do
+  before (:each) do
+    user = Factory(:user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password,  :with => user.password
+    click_button
+  end
+  
+  describe "Creation" do
+    describe "failure" do
+      it "should not make a new micropost" do
+        lambda do
+          visit root_path
+          fill_in :micropost_content, :with => ""
+          click_button
+          response.should render_template('pages/home')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(Micropost, :count)
+      end
+    end # failure
+    
+    describe "success" do
+      it "should make a new micropost" do
+        content = "Foo Bar"
+        lambda do
+          visit root_path
+          fill_in :micropost_content, :with => content
+          click_button
+          response.should render_template('pages/home')
+          response.should have_selector("td", :content => content)
+        end.should change(Micropost, :count).by(1)
+      end
+    end # success
+
+  end # Creation
+end # "Microposts"
+
